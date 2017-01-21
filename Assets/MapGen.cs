cc9c7efa4ea6e3735ac12824ddda449ac5ad290c
Kolppan3n <Kolppan3n@gmail.com>
2017-01-21 14:59:14 +0200

    mappeja ja mappigeni

---
+++ Assets/MapGen.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MapGen : MonoBehaviour {
+
+    private string Map = "Neighbourhoods";
+    GameObject[] Blocks;
+    GameObject curBlock;
+
+    void Start () {
+
+        //Lataa palikat listaan riippuen minkä kentän haluaa
+        Blocks = Resources.LoadAll<GameObject>("Prefabs/" + Map);
+        curBlock = Blocks[Random.Range(0, Blocks.Length)];
+        curBlock = Instantiate(curBlock, gameObject.transform.position, Quaternion.identity) as GameObject;
+
+        SpawnBlock();
+        SpawnBlock();
+        SpawnBlock();
+        SpawnBlock();
+        SpawnBlock();
+    }
+
+    void SpawnBlock()
+    {
+        //Kahtoo ettei tuu kahta samanlaista palikkaa putkeen
+        GameObject temp = Blocks[Random.Range(0, Blocks.Length)];
+        while (temp == curBlock) {
+            temp = Blocks[Random.Range(0, Blocks.Length)];
+        }
+
+        //Eka kahtoo mihinkä laitetaan ja sitte spawnaa uutena palikkana
+        Vector3 pos = curBlock.transform.FindChild("Endpoint").position;
+        curBlock = Instantiate(temp, pos, Quaternion.identity) as GameObject;
+    }
+}
