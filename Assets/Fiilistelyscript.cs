5f552f1a1b1db9ed8e8659a84be018b24814c04c
Kolppan3n <Kolppan3n@gmail.com>
2017-01-21 17:32:47 +0200

    en ees muista mitä tein :1234:

--- Assets/Fiilistelyscript.cs
+++ Assets/Fiilistelyscript.cs
@@ -3,7 +3,27 @@ using System.Collections;
 
 public class Fiilistelyscript : MonoBehaviour {
 
+    public float playerSpeed = 20;
+
+    float horizontalSpeed = 2f;
+    float verticalSpeed = 2f;
+
+    float yaw = 0f;
+    float pitch = 0f;
+
 	void Update () {
-        gameObject.transform.Translate(Vector3.forward * .2f);
+
+        //Liikuttaa pelaajaa
+        //gameObject.transform.Translate(Vector3.right * playerSpeed / 100, Space.World);
+
+        RotateCamera();
 	}
+
+    void RotateCamera()
+    {
+        yaw += horizontalSpeed * Input.GetAxis("Mouse X");
+        pitch -= verticalSpeed * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0f);
+    }
 }
