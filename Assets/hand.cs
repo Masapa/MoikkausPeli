c3bdc641ccfa6b0a78cb2c03f5685b67bb363e9b
Matti Passoja <matti@passoja.fi>
2017-01-21 13:45:12 +0200

    Käsi jotenki toimii

---
+++ Assets/hand.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hand : MonoBehaviour {
+    Rigidbody shoulder;
+    Rigidbody elbow;
+    Rigidbody wrist;
+
+	// Use this for initialization
+	void Start () {
+
+    
+     Transform[] transsi = gameObject.GetComponentsInChildren<Transform>();
+     foreach (Transform t in transsi)
+ {
+// Debug.Log(t.name);
+     if(t.name == "shoulder")
+     {
+     Debug.Log("osui");
+     shoulder = t.GetComponent<Rigidbody>();
+     }
+     if(t.name == "elbow") {
+     elbow = t.GetComponent<Rigidbody>();
+     }
+     if(t.name == "wrist") {
+     wrist = t.GetComponent<Rigidbody>();
+     }
+ }
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+    Debug.Log("Shoulder: "+shoulder.rotation+"\nElbow"+elbow.rotation);
+    if(Input.GetKey(KeyCode.A)) {
+    //Debug.Log("asd");
+    shoulder.velocity = Vector3.up * 5;
+   
+    }
+
+      if(Input.GetKey(KeyCode.S)) {
+    elbow.velocity = Vector3.up * 5;
+    }
+
+      if(Input.GetKey(KeyCode.D)) {
+    // * 100);
+    }
+	
+
+    if(Input.GetKey(KeyCode.Space)) {
+
+
+    }
+
+	}
+}
