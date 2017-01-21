285608ed4d41fbfe8651b642bae86e83b299b895
Kolppan3n <Kolppan3n@gmail.com>
2017-01-21 23:37:02 +0200

    jaa

---
+++ Assets/ButtonScripts.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScripts : MonoBehaviour {
+
+	public void LoadLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,25 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class Fiilistelyscript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour {
 
     public float playerSpeed = 20;
 
     float horizontalSpeed = 2f;
     float verticalSpeed = 2f;
-
     float maxYaw = 30;
     float minYaw = -30;
     float maxPitch = 13;
     float minPitch = -13;
-
     float yaw = 0f;
     float pitch = 0f;
+
+    List<Sprite> ImageList = new List<Sprite>();
 
 	void Update () {
 
         //Liikuttaa pelaajaa
         gameObject.transform.Translate(Vector3.right * playerSpeed / 100, Space.World);
+        ImageList.Add(Resources.Load<Sprite>("Sprites/uispritesheet01/uispritesheet01_31"));
+        Debug.Log(ImageList[0]);
 
         RotateCamera();
 	}
@@ -31,8 +34,6 @@ public class Fiilistelyscript : MonoBehaviour {
 
         pitch -= verticalSpeed * Input.GetAxis("Mouse Y");
         pitch = Mathf.Clamp(pitch, minPitch, maxPitch);
-
-        Debug.Log("Yaw: " + yaw + "  " + "Pitch: " + pitch);
 
         transform.localEulerAngles = new Vector3(pitch, yaw + 90, 0);
     }
